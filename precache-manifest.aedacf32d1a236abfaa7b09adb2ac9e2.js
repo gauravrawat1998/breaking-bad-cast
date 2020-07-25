3912b3868a9bc6ae3647e2bd19f82076a3f20562
gauravrawat1998 <gauravrawat2025@gmail.com>
2020-07-25 12:48:43 +0530

    Updates

---
+++ precache-manifest.aedacf32d1a236abfaa7b09adb2ac9e2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a90350c49870df672bdf66f03d932ab",
+    "url": "/breaking-bad-cast/index.html"
+  },
+  {
+    "revision": "e110fabd221ed57308ea",
+    "url": "/breaking-bad-cast/static/css/main.c42c75f4.chunk.css"
+  },
+  {
+    "revision": "cc7393bf9bda9a3b7340",
+    "url": "/breaking-bad-cast/static/js/2.417f8590.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-cast/static/js/2.417f8590.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e110fabd221ed57308ea",
+    "url": "/breaking-bad-cast/static/js/main.6391f908.chunk.js"
+  },
+  {
+    "revision": "aceb973de7feae88a752",
+    "url": "/breaking-bad-cast/static/js/runtime-main.039fb2eb.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
